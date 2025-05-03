f42ad80b80599aa9ebfe1c4079a78570cfc50eff
Mateusdev3 <mateusesteves33@gmail.com>
2025-05-02 22:05:25 -0300

    update 1.1

--- Server Manager/MainWindow.xaml.cs
+++ Server Manager/MainWindow.xaml.cs
@@ -8,21 +8,62 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.IO;
 
-namespace Server_Manager
-{
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
-    {
-        public MainWindow()
-        {
+namespace Server_Manager {
+
+    public partial class MainWindow : Window {
+
+        //Variaveis globais
+
+        string dayzexename = "DayZServer_x64.exe";
+        
+
+        public MainWindow() {
             InitializeComponent();
+
         }
 
-        private void ch1_Checked(object sender, RoutedEventArgs e) {
+        //Gerenciamento da janela principal
+
+        private void Mouse(object sender, MouseEventArgs e) {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                this.DragMove();
+            }
+        }
+        private void Fechar_Janela(object sender, RoutedEventArgs e) {
+
+            this.Close();
+
+        }
+
+        private void Minimizar_Janela(object sender, RoutedEventArgs e) {
+            this.WindowState = WindowState.Minimized;
+        }
+
+
+
+        //Botões principais
+
+        private void Iniciar(object sender, RoutedEventArgs e) {
+            if (File.Exists(dayzexename)){
+
+
+                
+            
+            }
+            else
+            {
+                MessageBox.Show(".exe do servidor não encontrado...");
+            }
+            
+
+
+
+
 
         }
     }
 }
+
